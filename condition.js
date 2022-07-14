f0fc506b379b6fb93ba44adebf9c812ab9ea31e9
SM Feroj <sm.feroj@gmail.com>
2022-07-14 07:41:04 +0600

    first commit

---
+++ condition.js
@@ -0,0 +1,12 @@
+// ==================if, else if and else============
+
+
+let number = 0;
+
+if (number>0){
+    console.log('positive')
+}else if (number<0){
+    console.log('negative')
+} else{
+    console.log('number is zero')
+};
